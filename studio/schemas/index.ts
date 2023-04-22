b6e24382dce7a93dd4d4114f9eb856ccfb3133dd
Martijn Dorsman <martijn.dorsman@gmail.com>
2023-04-22 14:06:00 +0200

    Install Sanity

---
+++ studio/schemas/index.ts
@@ -0,0 +1,5 @@
+import blockContent from './blockContent'
+import category from './category'
+import photo from './photo'
+
+export const schemaTypes = [photo, category, blockContent]
---
+++ studio/schemas/photo.ts
@@ -0,0 +1,33 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'photo',
+  title: 'Photo',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      category: 'category.title',
+      media: 'mainImage',
+    },
+  },
+})
